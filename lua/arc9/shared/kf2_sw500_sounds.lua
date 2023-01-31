c8b0d0cf80e9028b6b27597a88eeaada61fb8281
Pacagma <branimir.pekic@gmail.com>
2023-01-31 20:33:00 +0100

    SW500 Progress

---
+++ lua/arc9/shared/kf2_sw500_sounds.lua
@@ -0,0 +1,88 @@
+sound.Add( {
+    name = "KF2.SW500.ShootOutDoor",
+    channel = 10,
+    volume = {100,120},
+    level = 80,
+    --pitch = {97,103},
+	soundlevel = SNDLVL_GUNFIRE,
+    sound = {
+        "kf2/sw500/ShootOutDoor/ShootOutDoor1.ogg",
+        "kf2/sw500/ShootOutDoor/ShootOutDoor2.ogg",
+        "kf2/sw500/ShootOutDoor/ShootOutDoor3.ogg",
+        "kf2/sw500/ShootOutDoor/ShootOutDoor4.ogg",
+        "kf2/sw500/ShootOutDoor/ShootOutDoor5.ogg",
+        "kf2/sw500/ShootOutDoor/ShootOutDoor6.ogg",
+        "kf2/sw500/ShootOutDoor/ShootOutDoor7.ogg",
+        "kf2/sw500/ShootOutDoor/ShootOutDoor8.ogg",
+        "kf2/sw500/ShootOutDoor/ShootOutDoor9.ogg",
+        "kf2/sw500/ShootOutDoor/ShootOutDoor10.ogg",
+    }
+} )
+sound.Add( {
+    name = "KF2.SW500.ShootInDoor",
+    channel = 10,
+    volume = {100,120},
+    level = 80,
+    --pitch = {97,103},
+	soundlevel = SNDLVL_GUNFIRE,
+    sound = {
+        "kf2/sw500/ShootInDoor/ShootInDoor1.ogg",
+        "kf2/sw500/ShootInDoor/ShootInDoor2.ogg",
+        "kf2/sw500/ShootInDoor/ShootInDoor3.ogg",
+        "kf2/sw500/ShootInDoor/ShootInDoor4.ogg",
+        "kf2/sw500/ShootInDoor/ShootInDoor5.ogg",
+        "kf2/sw500/ShootInDoor/ShootInDoor6.ogg",
+        "kf2/sw500/ShootInDoor/ShootInDoor7.ogg",
+        "kf2/sw500/ShootInDoor/ShootInDoor8.ogg",
+        "kf2/sw500/ShootInDoor/ShootInDoor9.ogg",
+        "kf2/sw500/ShootInDoor/ShootInDoor10.ogg",
+        "kf2/sw500/ShootInDoor/ShootInDoor11.ogg",
+        "kf2/sw500/ShootInDoor/ShootInDoor12.ogg",
+        "kf2/sw500/ShootInDoor/ShootInDoor13.ogg",
+        "kf2/sw500/ShootInDoor/ShootInDoor14.ogg",
+    }
+} )
+sound.Add( {
+    name = "KF2.SW500.DistantShoot",
+    channel = CHAN_WEAPON,
+    volume = 200,
+    level = 130,
+    --pitch = {97,103},
+	soundlevel = SNDLVL_GUNFIRE,
+    sound = {
+        "kf2/sw500/ShootDistant/ShootDistant1.ogg",
+        "kf2/sw500/ShootDistant/ShootDistant2.ogg",
+        "kf2/sw500/ShootDistant/ShootDistant3.ogg",
+        "kf2/sw500/ShootDistant/ShootDistant4.ogg",
+        "kf2/sw500/ShootDistant/ShootDistant5.ogg",
+        "kf2/sw500/ShootDistant/ShootDistant6.ogg",
+        "kf2/sw500/ShootDistant/ShootDistant7.ogg",
+        "kf2/sw500/ShootDistant/ShootDistant8.ogg",
+    }
+} )
+sound.Add( {
+    name = "KF2.SW500.DistantShootInDoor",
+    channel = CHAN_WEAPON,
+    volume = 200,
+    level = 130,
+    --pitch = {97,103},
+	soundlevel = SNDLVL_GUNFIRE,
+    sound = {
+        "kf2/sw500/ShootDistantInDoor/ShootDistantInDoor1.ogg",
+        "kf2/sw500/ShootDistantInDoor/ShootDistantInDoor2.ogg",
+        "kf2/sw500/ShootDistantInDoor/ShootDistantInDoor3.ogg",
+        "kf2/sw500/ShootDistantInDoor/ShootDistantInDoor4.ogg",
+        "kf2/sw500/ShootDistantInDoor/ShootDistantInDoor5.ogg",
+        "kf2/sw500/ShootDistantInDoor/ShootDistantInDoor6.ogg",
+    }
+} )
+sound.Add( {
+    name = "KF2.SW500.DryShoot",
+    channel = CHAN_STATIC,
+    volume = 0.3,
+    --level = 100,
+    --pitch = {97,103},
+    sound = {
+        "kf2/sw500/DryFire/DryFire.ogg",
+    }
+} )
